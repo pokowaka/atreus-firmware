012bf1c96c0c919aa340028d42c804ba7dc740d9
Phil Hagelberg <phil@hagelb.org>
2015-03-12 10:33:49 +0700

    Fix bootloader jump on teensy.

--- keymap_common.c
+++ keymap_common.c
@@ -34,9 +34,20 @@ void bootloader() {
   clear_keyboard();
   print("\n\nJump to bootloader... ");
   _delay_ms(250);
-#ifndef TEENSY
+#ifdef TEENSY
+  cli();
+  UDCON = 1;
+  USBCON = (1<<FRZCLK);  // disable USB
+  UCSR1B = 0;
+  _delay_ms(5);
+  EIMSK = 0; PCICR = 0; SPCR = 0; ACSR = 0; EECR = 0; ADCSRA = 0;
+  TIMSK0 = 0; TIMSK1 = 0; TIMSK3 = 0; TIMSK4 = 0; UCSR1B = 0; TWCR = 0;
+  DDRB = 0; DDRC = 0; DDRD = 0; DDRE = 0; DDRF = 0; TWCR = 0;
+  PORTB = 0; PORTC = 0; PORTD = 0; PORTE = 0; PORTF = 0;
+  asm volatile("jmp 0x7E00");
+#else
   *(uint16_t *)0x0800 = 0x7777; // these two are a-star-specific
-#endif
-  bootloader_jump(); // doesn't actually work ATM
+  bootloader_jump();
   print("not supported.\n");
+#endif
 }
