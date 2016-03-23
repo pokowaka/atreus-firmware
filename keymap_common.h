ccc8f143618ff4e43a32c5d56466eca90804b832
George Madrid <gmadrid@gmail.com>
2016-03-23 14:45:39 -0400

    PCB flipping code didn't actually flip the keys.

--- keymap_common.h
+++ keymap_common.h
@@ -44,10 +44,10 @@ extern const uint16_t fn_actions[];
   K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, \
   K30, K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A \
 ) {                                                                     \
-  { K00, K01, K02, K03, K04, KC_NO, K05, K06, K07, K08, K09 }, \
-  { K10, K11, K12, K13, K14, KC_NO, K15, K16, K17, K18, K19 }, \
-  { K20, K21, K22, K23, K24, K35,   K25, K26, K27, K28, K29 }, \
-  { K2A, K30, K31, K32, K33, K34,   K36, K37, K38, K39, K3A } \
+  { K09, K08, K07, K06, K05, KC_NO, K04, K03, K02, K01, K00 }, \
+  { K19, K18, K17, K16, K15, KC_NO, K14, K13, K12, K11, K10 }, \
+  { K29, K28, K27, K26, K25, K35,   K24, K23, K22, K21, K20 }, \
+  { K3A, K39, K38, K37, K36, K34,   K33, K32, K31, K30, K2A } \
 }
 
 #define KEYMAP_PCBUP( \
